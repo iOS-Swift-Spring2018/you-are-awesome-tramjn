9835df757fc43c4760bb6b828676578db6b86cd5
Tram Nguyen <tramnguyen@trams-mbp.bc.edu>
2018-01-22 15:07:54 -0500

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Tram Nguyen on 1/22/18.
+//  Copyright © 2018 Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
